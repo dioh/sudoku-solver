a31a022e80854ed4761feb7ab14c3d37ea58976b
danito <fdaniel@infobytesec.com>
2014-12-05 23:53:02 -0300

    Isnt working but has lots of bugfix

--- scripts/solver.R
+++ scripts/solver.R
@@ -3,55 +3,66 @@ sudoku = read.csv('~/proj/sudoku-solver/testdata/sudoku1', header=FALSE)
 delta = function (x) { length(x) - length(unique(x)) + 1}
 evaluation = function(mat){ sum(apply(mat, MARGIN=1, delta) + apply(mat, MARGIN=2, delta)) }
 
-gen_neighbourgh = function(mat){
-  print(as.list(match.call()))
-    print('entre')
-    print(dim(mat))
-    N2 = dim(mat)[1]
-    print(paste('N2', as.character( N2)))
-    N = sqrt(N2)
-    # get i, j
-    i_j = sample(1:N2, 2)
-    print('random ij')
-    print(i_j)
-    # obtengo el subcuadrado de i_j
-    # ranges_start = ((i_j %% N) * 3) + 1
-    ranges_start = ((i_j %/% N) * N  ) 
-    print('sub cuadrante')
-    print(ranges_start)
-    ranges_start = replace(ranges_start,  ranges_start==0, 1)
-    ranges_start <- replace(ranges_start,  ranges_start==N2, N2 - N) 
-    print('ranges =')
-    print(ranges_start)
-    l = sample(seq(from=ranges_start[1], length.out = N), 1)
-    k = sample(seq(from=ranges_start[2], length.out = N), 1)
-    print( i_j)
-    print(paste('[', l, ', ', k, ']'))
-    if(mat[i_j[1], i_j[2]] == mat[l, k])
-        return(gen_neighbourgh(mat))
-    tmp = mat[i_j[1], i_j[2]]
-    mat[i_j[1], i_j[2]] = mat[l, k]
-    mat[l, k] = tmp
-    #     print(paste('Cambiando [', i_j[0], i_j[2], '] por [', l, k, ']') )
-    return (mat)
+gen_neighbourgh = function(fixed_cells){
+  print(fixed_cells)
+    mat = sudoku
+    for (repeats in seq(1,fixed_cells)) {
+        N2 = dim(mat)[1]
+        N = sqrt(N2)
+        i_j = sample(1:N2, 2)
+        l_k = get_next_index(i_j)
+        # print('cambio')
+        # print(c(i_j))
+        # print('por')
+        # print(c(l_k))
+        if(all(i_j == l_k))
+            gen_neighbourgh(1)
+        tmp = mat[i_j[1], i_j[2]]
+        mat[i_j[1], i_j[2]] = mat[l_k[1], l_k[2]]
+        mat[l_k[1], l_k[2]] = tmp
+        assign("sudoku", mat, envir = .GlobalEnv)
+    }
+    return (evaluation(mat))
+}
+get_next_index = function(i_j){ 
+    # 1, 2, 3 => 1
+    # 4, 5, 6 => 4
+    # 7, 8, 9 => 7
+
+    replaces = c(1,1,1,4,4,4,7,7,7)
+
+    ranges_start = c(replaces[i_j[1]], replaces[i_j[2]])
+    l = sample(seq(from=ranges_start[1], length.out = N ), 1)
+    k = sample(seq(from=ranges_start[2], length.out = N ), 1)
+    return(c(l,k))
 }
 
 random_zero_replace_asign = function(mat){
   N2 = dim(mat)[1]
-  ran_no_cero = apply(
-    mat, MARGIN = c(1,2),
-    function(x) ifelse(x==0, sample(1:N2, 1), x))
-  return(ran_no_cero)
+  N=sqrt(N2)
+  
+  for(i in c(1,4,7)){
+    for (j in c(1,4,7)){
+      square = mat[seq(i,i+(N-1)),seq(j,j+(N-1))]
+      used_nums = square[square>0]
+      all_nums = 1:N2
+      fillers = all_nums[!all_nums %in% used_nums]
+      square[square == 0] = sample(fillers, length(fillers))
+      mat[seq(i,i+(N-1)),seq(j,j+(N-1))] = square
+    }
+  }
+  return(mat)
 }
 # objte
 
 
 eval_next = function(mat) {
   nextt= gen_neighbourgh(matrix(mat, 9, 9))
-  print(dim(nextt))
+  # # print(dim(nextt))
   eval = evaluation(nextt)
   return(eval)
 }
 
-sudoku = apply(sudoku, 1:2, as.double)
-GenSA(par=random_zero_replace_asign(sudoku), fn=eval_next, lower = rep(1, 81), upper = rep(9, 81))
+ sudoku = apply(sudoku, 1:2, as.double)
+# GenSA(par=random_zero_replace_asign(sudoku),
+#       fn=eval_next, lower = rep(1, 81), upper = rep(9, 81))
